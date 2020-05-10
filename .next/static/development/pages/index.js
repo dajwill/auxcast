cc9e840073f70da457b70844ee319c47db6a554e
David Williams <dajwilliam@gmail.com>
2020-05-10 11:21:59 -0400

    add next.js

---
+++ .next/static/development/pages/index.js
@@ -0,0 +1,85 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/index.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fdajwill%2FProjects%2Fauxcast%2Fsrc%2Fpages%2Findex.tsx&hotRouterUpdates=true!./":
+/*!****************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fdajwill%2FProjects%2Fauxcast%2Fsrc%2Fpages%2Findex.tsx&hotRouterUpdates=true ***!
+  \****************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P = window.__NEXT_P || []).push([
+      "/",
+      function () {
+        var mod = __webpack_require__(/*! ./src/pages/index.tsx */ "./src/pages/index.tsx");
+        if (true) {
+          module.hot.accept(/*! ./src/pages/index.tsx */ "./src/pages/index.tsx", function () {
+            if (!next.router.components["/"]) return;
+            var updatedPage = __webpack_require__(/*! ./src/pages/index.tsx */ "./src/pages/index.tsx");
+            next.router.update("/", updatedPage);
+          });
+        }
+        return mod;
+      }
+    ]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_0fb095e325d7ebf261c3 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_0fb095e325d7ebf261c3 */ "dll-reference dll_0fb095e325d7ebf261c3"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./src/pages/index.tsx":
+/*!*****************************!*\
+  !*** ./src/pages/index.tsx ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+
+function HomePage() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, "Welcome to Next.js!");
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (HomePage);
+
+/***/ }),
+
+/***/ 1:
+/*!********************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fdajwill%2FProjects%2Fauxcast%2Fsrc%2Fpages%2Findex.tsx&hotRouterUpdates=true ***!
+  \********************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fdajwill%2FProjects%2Fauxcast%2Fsrc%2Fpages%2Findex.tsx&hotRouterUpdates=true! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fdajwill%2FProjects%2Fauxcast%2Fsrc%2Fpages%2Findex.tsx&hotRouterUpdates=true!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_0fb095e325d7ebf261c3":
+/*!*******************************************!*\
+  !*** external "dll_0fb095e325d7ebf261c3" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_0fb095e325d7ebf261c3;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=index.js.map
